cacdb5d3acd9acc73f925b3d910d06029beeab51
Ignacio Tartavull <tartavull@princeton.edu>
2015-06-26 19:03:53 -0400

    using yo to add express

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('galleryApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
