64cbff5ff021a9a60c43edffb4b17f3176458884
Ubuntu <ubuntu@ip-10-0-0-175.ec2.internal>
2015-07-14 18:59:00 +0000

    added install.sh to install depencies required for ubuntu

--- README.md
+++ README.md
@@ -2,13 +2,5 @@
 Repository for the museum.
 
 ## Dependencies
-* sudo npm install -g grunt  grunt-cli bower
-* bower install
-* npm install
-* sudo apt-get install ruby-full mongodb-server
-* gem install sass
-
-## Running the server
-* grunt serve
-
+run ./install.sh
 
---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install npm
+sudo npm -g install n
+sudo n stable
+sudo npm install -g npm
+sudo npm -g install bower grunt grunt-cli
+sudo apt-get install ruby-full
+sudo gem install sass
+sudo apt-get install mongodb-server
+sudo apt-get install git
+sudo apt-get install ruby-full
+sudo gem install sass
+git clone https://github.com/seung-lab/gallery.git
+cd gallery/
+npm install
+bower install
+grunt serve
+
