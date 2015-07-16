0a75d9f5ac67536e540af712077ca47c3992799d
Ignacio Tartavull <tartavull@princeton.edu>
2015-07-16 18:10:40 -0400

    refractor of ctmFactory

--- client/app/factories/ctmFactory.js
+++ client/app/factories/ctmFactory.js
@@ -13,11 +13,9 @@ app.factory("ctmFactory", [function () {
 
       var file = event.data;
       
-      createModelBuffers( file , function(geometry) {
+      var geometry = new Model(file);
 
-        scope.callback(geometry);
-
-      });   
+      callback(geometry);
 
     };
 
@@ -25,231 +23,208 @@ app.factory("ctmFactory", [function () {
 
   }
   ctm.prototype.constructor = ctm;
+    
+  var Model = function ( file ) {
 
+    this.initializeBuffers(file);
 
+    this.reorderVertices();
+    
+    this.computeDrawCalls();
 
-  var createModelBuffers = function ( file, callback ) {
+    this.addAttributes();
+  
+    return this.geometry;
+  };
 
-    var Model = function ( ) {
+  Model.prototype.initializeBuffers = function(file) {
+    
+    this.geometry =  new THREE.BufferGeometry(file);
 
-      var scope = this;
+    // init GL buffers
 
-      var reorderVertices = true;
+    this.vertexIndexArray = file.body.indices;
+    this.vertexPositionArray = file.body.vertices;
+    this.vertexNormalArray = file.body.normals;
 
-      scope.materials = [];
 
-      THREE.BufferGeometry.call( this );
+    if ( file.body.uvMaps !== undefined && file.body.uvMaps.length > 0 ) {
 
-      // init GL buffers
-
-      var vertexIndexArray = file.body.indices,
-      vertexPositionArray = file.body.vertices,
-      vertexNormalArray = file.body.normals;
-
-      var vertexUvArray, vertexColorArray;
-
-      if ( file.body.uvMaps !== undefined && file.body.uvMaps.length > 0 ) {
-
-        vertexUvArray = file.body.uvMaps[ 0 ].uv;
-
-      }
-
-      if ( file.body.attrMaps !== undefined && file.body.attrMaps.length > 0 && file.body.attrMaps[ 0 ].name === "Color" ) {
-
-        vertexColorArray = file.body.attrMaps[ 0 ].attr;
-
-      }
-
-      // reorder vertices
-      // (needed for buffer splitting, to keep together face vertices)
-
-      if ( reorderVertices ) {
-
-        var newFaces = new Uint32Array( vertexIndexArray.length ),
-          newVertices = new Float32Array( vertexPositionArray.length );
-
-        var newNormals, newUvs, newColors;
-
-        if ( vertexNormalArray ) newNormals = new Float32Array( vertexNormalArray.length );
-        if ( vertexUvArray ) newUvs = new Float32Array( vertexUvArray.length );
-        if ( vertexColorArray ) newColors = new Float32Array( vertexColorArray.length );
-
-        var indexMap = {}, vertexCounter = 0;
-
-        function handleVertex( v ) {
-
-          if ( indexMap[ v ] === undefined ) {
-
-            indexMap[ v ] = vertexCounter;
-
-            var sx = v * 3,
-              sy = v * 3 + 1,
-              sz = v * 3 + 2,
-
-              dx = vertexCounter * 3,
-              dy = vertexCounter * 3 + 1,
-              dz = vertexCounter * 3 + 2;
-
-            newVertices[ dx ] = vertexPositionArray[ sx ];
-            newVertices[ dy ] = vertexPositionArray[ sy ];
-            newVertices[ dz ] = vertexPositionArray[ sz ];
-
-            if ( vertexNormalArray ) {
-
-              newNormals[ dx ] = vertexNormalArray[ sx ];
-              newNormals[ dy ] = vertexNormalArray[ sy ];
-              newNormals[ dz ] = vertexNormalArray[ sz ];
-
-            }
-
-            if ( vertexUvArray ) {
-
-              newUvs[ vertexCounter * 2 ]   = vertexUvArray[ v * 2 ];
-              newUvs[ vertexCounter * 2 + 1 ] = vertexUvArray[ v * 2 + 1 ];
-
-            }
-
-            if ( vertexColorArray ) {
-
-              newColors[ vertexCounter * 4 ]     = vertexColorArray[ v * 4 ];
-              newColors[ vertexCounter * 4 + 1 ] = vertexColorArray[ v * 4 + 1 ];
-              newColors[ vertexCounter * 4 + 2 ] = vertexColorArray[ v * 4 + 2 ];
-              newColors[ vertexCounter * 4 + 3 ] = vertexColorArray[ v * 4 + 3 ];
-
-            }
-
-            vertexCounter += 1;
-
-          }
-
-        }
-
-        var a, b, c;
-
-        for ( var i = 0; i < vertexIndexArray.length; i += 3 ) {
-
-          a = vertexIndexArray[ i ];
-          b = vertexIndexArray[ i + 1 ];
-          c = vertexIndexArray[ i + 2 ];
-
-          handleVertex( a );
-          handleVertex( b );
-          handleVertex( c );
-
-          newFaces[ i ]     = indexMap[ a ];
-          newFaces[ i + 1 ] = indexMap[ b ];
-          newFaces[ i + 2 ] = indexMap[ c ];
-
-        }
-
-        vertexIndexArray = newFaces;
-        vertexPositionArray = newVertices;
-
-        if ( vertexNormalArray ) vertexNormalArray = newNormals;
-        if ( vertexUvArray ) vertexUvArray = newUvs;
-        if ( vertexColorArray ) vertexColorArray = newColors;
-
-      }
-
-      // compute offsets
-
-      scope.offsets = [];
-
-      var indices = vertexIndexArray;
-
-      var start = 0,
-        min = vertexPositionArray.length,
-        max = 0,
-        minPrev = min;
-
-      for ( var i = 0; i < indices.length; ) {
-
-        for ( var j = 0; j < 3; ++ j ) {
-
-          var idx = indices[ i ++ ];
-
-          if ( idx < min ) min = idx;
-          if ( idx > max ) max = idx;
-
-        }
-
-        if ( max - min > 65535 ) {
-
-          i -= 3;
-
-          for ( var k = start; k < i; ++ k ) {
-
-            indices[ k ] -= minPrev;
-
-          }
-
-          scope.offsets.push( { start: start, count: i - start, index: minPrev } );
-
-          start = i;
-          min = vertexPositionArray.length;
-          max = 0;
-
-        }
-
-        minPrev = min;
-
-      }
-
-      for ( var k = start; k < i; ++ k ) {
-
-        indices[ k ] -= minPrev;
-
-      }
-
-      scope.offsets.push( { start: start, count: i - start, index: minPrev } );
-
-      // recast CTM 32-bit indices as 16-bit WebGL indices
-
-      var vertexIndexArray16 = new Uint16Array( vertexIndexArray );
-
-      // attributes
-
-      var attributes = scope.attributes;
-
-      attributes[ "index" ]    = { itemSize: 1, array: vertexIndexArray16, numItems: vertexIndexArray16.length };
-      attributes[ "position" ] = { itemSize: 3, array: vertexPositionArray, numItems: vertexPositionArray.length };
-
-      if ( vertexNormalArray !== undefined ) {
-
-        attributes[ "normal" ] = { itemSize: 3, array: vertexNormalArray, numItems: vertexNormalArray.length };
-
-      }
-
-      if ( vertexUvArray !== undefined ) {
-
-        attributes[ "uv" ] = { itemSize: 2, array: vertexUvArray, numItems: vertexUvArray.length };
-
-      }
-
-      if ( vertexColorArray !== undefined ) {
-
-        attributes[ "color" ]  = { itemSize: 4, array: vertexColorArray, numItems: vertexColorArray.length };
-
-      }
+      this.vertexUvArray = file.body.uvMaps[ 0 ].uv;
 
     }
 
-    Model.prototype = Object.create( THREE.BufferGeometry.prototype );
+    if ( file.body.attrMaps !== undefined && file.body.attrMaps.length > 0 && file.body.attrMaps[ 0 ].name === "Color" ) {
 
-    var geometry = new Model();
-
-    // compute vertex normals if not present in the CTM model
-
-    if ( geometry.attributes[ "normal" ] === undefined ) {
-
-      geometry.computeVertexNormals();
+      this.vertexColorArray = file.body.attrMaps[ 0 ].attr;
 
     }
-
-    callback( geometry );
 
   };
 
+  Model.prototype.reorderVertices = function() {
+    // reorder vertices
+    // (needed for buffer splitting, to keep together face vertices)
+  
+    var newFaces = new Uint32Array( this.vertexIndexArray.length );
+    var newVertices = new Float32Array( this.vertexPositionArray.length );
+
+    var newNormals, newUvs, newColors;
+
+    if ( this.vertexNormalArray ) newNormals = new Float32Array( this.vertexNormalArray.length );
+    if ( this.vertexUvArray ) newUvs = new Float32Array( this.vertexUvArray.length );
+    if ( this.vertexColorArray ) newColors = new Float32Array( this.vertexColorArray.length );
+
+    var indexMap = {}, vertexCounter = 0;
+
+    var scope = this;
+    function handleVertex( v ) {
+
+      if ( indexMap[ v ] === undefined ) {
+
+        indexMap[ v ] = vertexCounter;
+
+        var sx = v * 3,
+          sy = v * 3 + 1,
+          sz = v * 3 + 2,
+
+          dx = vertexCounter * 3,
+          dy = vertexCounter * 3 + 1,
+          dz = vertexCounter * 3 + 2;
+
+        newVertices[ dx ] = scope.vertexPositionArray[ sx ];
+        newVertices[ dy ] = scope.vertexPositionArray[ sy ];
+        newVertices[ dz ] = scope.vertexPositionArray[ sz ];
+
+        if ( scope.vertexNormalArray ) {
+
+          newNormals[ dx ] = scope.vertexNormalArray[ sx ];
+          newNormals[ dy ] = scope.vertexNormalArray[ sy ];
+          newNormals[ dz ] = scope.vertexNormalArray[ sz ];
+
+        }
+
+        if ( scope.vertexUvArray ) {
+
+          newUvs[ vertexCounter * 2 ]   = this.vertexUvArray[ v * 2 ];
+          newUvs[ vertexCounter * 2 + 1 ] = this.vertexUvArray[ v * 2 + 1 ];
+
+        }
+
+        if ( scope.vertexColorArray ) {
+
+          newColors[ vertexCounter * 4 ]     = scope.vertexColorArray[ v * 4 ];
+          newColors[ vertexCounter * 4 + 1 ] = scope.vertexColorArray[ v * 4 + 1 ];
+          newColors[ vertexCounter * 4 + 2 ] = scope.vertexColorArray[ v * 4 + 2 ];
+          newColors[ vertexCounter * 4 + 3 ] = scope.vertexColorArray[ v * 4 + 3 ];
+
+        }
+
+        vertexCounter += 1;
+
+      }
+
+    }
+
+    var a, b, c;
+
+    for ( var i = 0; i < this.vertexIndexArray.length; i += 3 ) {
+
+      a = this.vertexIndexArray[ i ];
+      b = this.vertexIndexArray[ i + 1 ];
+      c = this.vertexIndexArray[ i + 2 ];
+
+      handleVertex( a );
+      handleVertex( b );
+      handleVertex( c );
+
+      newFaces[ i ]     = indexMap[ a ];
+      newFaces[ i + 1 ] = indexMap[ b ];
+      newFaces[ i + 2 ] = indexMap[ c ];
+
+    }
+
+    this.vertexIndexArray = newFaces;
+    this.vertexPositionArray = newVertices;
+
+    if ( this.vertexNormalArray ) this.vertexNormalArray = newNormals;
+    if ( this.vertexUvArray ) this.vertexUvArray = newUvs;
+    if ( this.vertexColorArray ) this.vertexColorArray = newColors;
+
+  };
+
+  Model.prototype.computeDrawCalls = function() {
+
+    var indices = this.vertexIndexArray;
+
+    var start = 0,
+      min = this.vertexPositionArray.length,
+      max = 0,
+      minPrev = min;
+
+    for ( var i = 0; i < indices.length; ) {
+
+      for ( var j = 0; j < 3; ++ j ) {
+
+        var idx = indices[ i ++ ];
+
+        if ( idx < min ) min = idx;
+        if ( idx > max ) max = idx;
+
+      }
+
+      if ( max - min > 65535 ) {
+
+        i -= 3;
+
+        for ( var k = start; k < i; ++ k ) {
+
+          indices[ k ] -= minPrev;
+
+        }
+
+        this.geometry.addDrawCall(start, i - start, minPrev );
+
+        start = i;
+        min = this.vertexPositionArray.length;
+        max = 0;
+
+      }
+
+      minPrev = min;
+
+    }
+
+    for ( var k = start; k < i; ++ k ) {
+
+      indices[ k ] -= minPrev;
+
+    }
+
+    this.geometry.addDrawCall( start, i - start, minPrev );
+
+  };
+
+  Model.prototype.addAttributes = function() {
+      
+    this.geometry.addAttribute( 'index' , new THREE.BufferAttribute(this.vertexIndexArray, 1));
+    this.geometry.addAttribute( 'position', new THREE.BufferAttribute( this.vertexPositionArray , 3));
+
+    if ( this.vertexNormalArray !== undefined ) {
+      this.geometry.addAttribute( 'normal' , new THREE.BufferAttribute(this.vertexNormalArray, 3));
+    } 
+    else {
+      this.geometry.computeVertexNormals();
+    }
+
+    if ( this.vertexUvArray !== undefined ) {
+      this.geometry.addAttribute( 'uv' , new THREE.BufferAttribute(this.vertexUvArray, 2));
+    }
+
+    if ( this.vertexColorArray !== undefined ) {
+      this.geometry.addAttribute( 'color' , new THREE.BufferAttribute(this.vertexColorArray, 4));
+    }
+  };
 
   return ctm;
 
