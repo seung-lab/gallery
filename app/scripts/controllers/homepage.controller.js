0576e530b725cb65e87b134e1339c73fd174b18b
William Silversmith <wms@mit.edu>
2016-05-02 17:19:31 -0400

    More messing with styles.

--- app/scripts/controllers/homepage.controller.js
+++ app/scripts/controllers/homepage.controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
 app.controller('HomepageCtrl', function ($scope, $timeout, $mdSidenav, $mdDialog, $log, $state) {
-
     $scope.goToSelector = function() {
         $state.go('selector')
     };
