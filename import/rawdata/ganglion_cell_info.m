8515bcfad8ec21111c26150ea81af41d092c4170
William Silversmith <william.silversmith@gmail.com>
2017-08-15 11:36:33 -0400

    feat: updated ganglion cell classifications

--- import/rawdata/ganglion_cell_info.m
+++ import/rawdata/ganglion_cell_info.m
@@ -1,171 +1,223 @@
 function gc=ganglion_cell_info()
 
 gc=[
+struct('name','1ni','annotation','','cells',[20164,17182,20132,26087,26019],...
+	'whole_cells',[20164,20132]); % 20164
+% gallery id: 20164
 
-% melanopsin: very large sparse
-struct('name','1ws','annotation','M1','cells',[20203 20029]); % 20203
-% skel
-struct('name','1wt','annotation','sOff\alpha','cells',[10018 17109 26022]); % 17109
-% melanopsin? 
-struct('name','1no','annotation','','cells',[20157 17110 17050 17021 20092 17236 26024 10007]); % 20092
-% 
-struct('name','1ni','annotation','','cells',[20164 17182 20132 26019 26087]); % 20164
-%
-% mini-j. outlier: 17130
-struct('name','2an','annotation','F-mini^{Off}','cells',[17177 20066 20060 17027 15018 17105 15066 10017 10010 17062 50001 20264 20168 20147 17130 20101 20024 26190 26026 26049 26082 26129 26147 26172 26041]);
-% 15066
-% 
-% midi-J: 17144(>>3o?) 17061 0201 26095 17200 26055 26189(>>3x?)
-struct('name','2aw','annotation','F-midi^{Off},J','cells',[17075 17060 17028 20103 20201 17205 17107 20047 17144 17061 17200 26018 26150 26163 26189 26193 26095 26055 17024 26003 26038 26110 26131]); 
-% 17028
-%
-struct('name','2o','annotation','','cells',[17216 10005 10013 26062 26118]);
-% 10005
-% suspicious: 17192 (26126<>26131)
-struct('name','2i','annotation','','cells',[50004 17013 17092 20051 20234 20082 26109 17192 26157 26126]);
-% 20234
-% 
-struct('name','3o','annotation','','cells',[26155 20121 17037 17076]);
-% 17037
-% mini tOffAlpha
-struct('name','3i','annotation','','cells',[17135 20107 17077 26063 26104 26116 26188]);
-% mini-sOFFalpha (was written above)
-% 17135
-%
-% 3x merged into 2aw
-% struct('name','3x','annotation','','cells',[26003 26038 26110 26131]);
-% 
+struct('name','1no','annotation','','cells',[26024,17021,17110,20157,20092,17236,10007,17050],...
+	'whole_cells',[17021,20092,17110,20157]); % 20092
+% gallery id: 20092
 
+% M1
+struct('name','1ws','annotation','M1','cells',[20203,20029],...
+	'whole_cells',[20203]); % 20203
+% gallery id: 20203
+
+% sOffalpha
+struct('name','1wt','annotation','sOff\alpha','cells',[10018,17109,26022],...
+	'whole_cells',[17109]); % 17109
+% gallery id: 17109
+
+% F-mini Off
+struct('name','2an','annotation','F-mini^{Off}','cells',[26147,15066,20147,10010,10017,50001,17105,20264,17177,26129,17027,20066,20024,17130,15018,26041,20060,26049,26026,26172,26082,26190,20101,20168,17062],...
+	'whole_cells',[10010,15066,17027,10017,17105,20060,20066,20101,20147,20168,20264,26026,26172,50001]);
+% gallery id: 15066
+
+% F-midi Off, J
+struct('name','2aw','annotation','F-midi^{Off},J','cells',[20103,17028,20201,20047,26038,17061,26163,17075,17060,17107,26055,26150,17200,26193,17205,26003,26095,26018,17024,26189,26110],...
+	'whole_cells',[17028,17107,20103]); 
+% gallery id: 17028
+
+struct('name','2i','annotation','','cells',[17144,26126,26131,17092,50004,20051,17192,20234,17013,26109,20082,26157],...
+	'whole_cells',[20051,20234,50004]);
+% gallery id: 20234
+
+struct('name','2o','annotation','','cells',[26118,10005,10013,26062,17216],...
+	'whole_cells',[26062]);
+% gallery id: 10005
+
+struct('name','25','annotation','','cells',[26031,26040,20104,26042,26145,20067,20237,17132,26117,26134,17176,26066,20186,25006,26175,20105,26037,20045,26060,20036,26167,26099],...
+	'whole_cells',[17132,20045,20036,20067,20104,20105,20186,20237,25006,26042,26167]);
+% gallery id: 17132
+
+struct('name','27','annotation','','cells',[17212,26065,20117,26051],...
+	'whole_cells',[20117,26065]); 
+% gallery id: 26065
+
+struct('name','28','annotation','','cells',[20163,20155,20243,26005,20257],...
+	'whole_cells',[20163,20167]); 
+% gallery id: 20155
+
+struct('name','3i','annotation','','cells',[17135,20107,26116,26063,17077,26104],...
+	'whole_cells',[17135,20107]);
+% gallery id: 17135
+
+struct('name','3o','annotation','','cells',[26155,17076,17037,26188,20121],...
+	'whole_cells',[17037,20121]);
+% gallery id: 17037
+
+% On-Off DS caudal
+struct('name','37c','annotation','On-Off DS','cells',[20254,20245,20210,20179,20002,26036,26047,26056,26101,26137],...
+	'whole_cells',[20210,20179]); 
+% gallery id: 20210
+
+% On-Off DS dorsal
+struct('name','37d','annotation','On-Off DS','cells',[90002,20125,20014,26029,26094,26162],...
+	'whole_cells',[20125,20014]); 
+% gallery id: 20125
+
+% On-Off DS rostral
+struct('name','37r','annotation','On-Off DS','cells',[90001,17080,25005,20220,20213,26032,26084,26103,26165],...
+	'whole_cells',[20213,20220,25005,90001]); 
+% gallery id: 20213
+
+% On-Off DS ventral
+struct('name','37v','annotation','On-Off DS','cells',[17161,20233,20137,20096,20016,26115,26138,26158,26178],...
+	'whole_cells',[20233,20016,17161,20096]); 
+% gallery id: 20233
+
+struct('name','4i','annotation','','cells',[17022,26096,25004,20170,26008,26006,20174,17247,26164,26050,26102,17057],...
+	'whole_cells',[17022,20170,20174]); 
+% gallery id: 20170
 
 % mini-tOFFalpha
-struct('name','4on','annotation','','cells',[20230 17064 17034 17167 17151 20041 26021 26086 26121 26146 26160]); % 20230
-% 
-struct('name','4ow','annotation','tOff\alpha','cells',[17188 17079 20156 26004]); % 20156
-% 
-struct('name','4i','annotation','','cells',[25004 17022 20170 17247 20174 17057 26006 26008 26050 26096 26102 26164]); % 20170 
-%
+struct('name','4on','annotation','','cells',[26121,17167,26146,26021,20230,17064,26086,17151,20041,17034,26160],...
+	'whole_cells',[17167,20041,20230,26021]); 
+% gallery id: 20230
 
-% 
-struct('name','5to','annotation','','cells',[20128 20165 20240]); % 20240
-% w3 like
-struct('name','5ti','annotation','','cells',[17090 17181 50002 20114 20102 20216 20191 20184 20226 20262 20097 17093 17121 17159 20089 17190 17078 17059 20127 20055 26144 26170 26161 26156 26152 26123 26120 26112 26083 26053]);
-% 20184
-% 
-struct('name','5so','annotation','','cells',[17160 17146 20223 17081 17127 17168 20053 20012 26046 17138 17011 26111 26140 26151 26181 26187]);
-% 20223
-% 17011 17138
-struct('name','5si','annotation','','cells',[17040 20135 20183 17071 20070 17055 26044 26106 26159 26142 26133]); % 20183
-%  
+% tOffalpha
+struct('name','4ow','annotation','tOff\alpha','cells',[26004,20156,17079,17188],...
+	'whole_cells',[20156]); 
+% gallery id: 20156
+ 
+struct('name','5si','annotation','HD1','cells',[17040,26044,26142,26106,20135,17071,26133,17055,20070,20183],...
+	'whole_cells',[17040,20135,20183]); 
+%  gallery id: 20183
+
+struct('name','5so','annotation','HD2','cells',[17146,26140,17168,26046,20223,17127,26181,26187,17160,20053,26111,20012,26151,17081,26098,26159,26122],...
+	'whole_cells',[17011,17138,17168,17146,17160,20012,20053,20223,26046]);
+% gallery id: 20223
+
+struct('name','5ti','annotation','UHD','cells',[20262,20127,20184,17121,17190,26112,20191,17093,20226,20102,26144,26083,26161,20216,26170,26123,20114,26152,26156,20097,26053,17078,20055,20089,17059,26120,17159,50002,17090,17181],...
+	'whole_cells',[17121,17159,17190,20055,20089,20097,20102,20114,20127,20184,20191,20216,20226,20262,26161]);
+% gallery id: 20184
+
+struct('name','5to','annotation','','cells',[20165,20128,20240],...
+	'whole_cells',[20128,20240]); 
+% gallery id: 20240
+
+% W3 
+struct('name','51','annotation','W3/LED','cells',[20120,17012,20258,20182,26025,26039,26085,17098,26054,17138,20212,20037,17095,26113,26154,26177,26136,17011,20153,17035],...
+	'whole_cells',[17095,20037,20120,20153,20182,20212,26098,20258,26025]);
+% gallery id: 20212
+
+% F-mini On
+struct('name','63','annotation','F-mini^{On}','cells',[20178,17114,26125,20208,30003,20140,26148,20005,17140,26027,26089,26068,20181,20011,20129,26028,17097,26141,20019,20071,26191,17084,30002,26023,26057],...
+	'whole_cells',[17084,17114,17140,20005,20129,20071,20140,20178,20181,20208,26023,26028]);
+% gallery id: 20178
 
 % mini-tONalpha
-struct('name','6sn','annotation','','cells',[20198 20073 17082 26035 26043 26171]); % 20198
+struct('name','6sn','annotation','','cells',[17082,26171,26043,26035,20198,20073],...
+	'whole_cells',[20198,26043,20073]); 
+% gallery id: 20198
+
 % tOnalpha
-struct('name','6sw','annotation','','cells',[20222 20217 17083 20068 26020]); % 20222
-% 
-struct('name','6t','annotation','F-midi^{On}','cells',[20255 20113 20232]); % 20232
-% 
+struct('name','6sw','annotation','tOn\alpha','cells',[17083,20068,20222,26020,20217],...
+	'whole_cells',[20217,20222]); 
+% gallery id: 20222
 
-% onDSGC
-struct('name','7o','annotation','tOn DS','cells',[17053 20180 26034 26048 26100 26130 20239]); % 20239 
-% onDSGC
-% struct('name','7i','annotation','sON DS','cells',[17152 26077 20021 26002 26128 20075 26070 26075 26078]);
-struct('name','7iv','annotation','sOn DS','cells',[17152 26077]); % 26077 
-struct('name','7ir','annotation','sOn DS','cells',[20021 26002 26128 20075]); % 26002
-struct('name','7id','annotation','sOn DS','cells',[26070 26075 26078]); % 26078 
+% F-midi On 
+struct('name','6t','annotation','F-midi^{On}','cells',[20232,20255,20113],...
+	'whole_cells',[20113,20232]); 
+% gallery id: 20232
 
-% on alpha sustained/M4
-struct('name','8w','annotation','sON\alpha','cells',[17111 26001 26071 26079]); % 17111
+% On DS
+struct('name','7id','annotation','sOn DS','cells',[26070,26075,26078],...
+	'whole_cells',[26078]); 
+% gallery id: 26078
 
-% 9w
-% 
-struct('name','8n','annotation','','cells',[20126]);
-% 
-% struct('name','9m','annotation','','cells',[20112 20076 20056]);
-%
-% struct('name','9n','annotation','','cells',[26149 26168 26127 20006]);
-struct('name','9n','annotation','M5','cells',[26149 26168 26127 20006 20112 20076 20056 26074 26135]); % 20112
-% very wide 
-struct('name','9w','annotation','M2','cells',[20228]);
+% On DS
+struct('name','7ir','annotation','sOn DS','cells',[20075,20021,26002,26128],...
+	'whole_cells',[26002]); 
+% gallery id: 26002
 
+% On DS
+struct('name','7iv','annotation','sOn DS','cells',[17152,26077],...
+	'whole_cells',[26077]); 
+% gallery id: 26077 
 
-% w3: 
-struct('name','51','annotation','W3','cells',[20120 20212 20182 17098 17095 20153 20258 17035 20037 26177 26154 26136 26113 26098 26085 26054 26039 26025 26122]);
-% 20212
-% minor stratum at layer 5
-struct('name','25','annotation','','cells',[20105 20104 20186 20237 20067 25006 17176 17132 20045 20036 26031 26134 26167 26042 26040 26037 26060 26066 26099 26117 26145 26175]);
-% 17132
+% On DS
+struct('name','7o','annotation','tOn DS','cells',[17053,20239,20180,26034,26048,26100,26130],...
+	'whole_cells',[20180,20239,26048]); 
+% gallery id: 20239
 
-% trimodal
-struct('name','85','annotation','','cells',[20063 20072 20200 17012 17038 26061 26045 26092 20046 26119]); % 20200
+struct('name','72','annotation','','cells',[26124,17069,20074,20221,20166],...
+	'whole_cells',[20166,20221,17069]); 
+% gallery id: 20221
 
+struct('name','73','annotation','OND','cells',[20187,20100,20150,26132,26059,20043,26073],...
+	'whole_cells',[20100,20150,20187]); 
+% gallery id: 20100
 
-% ooDSGC. outliers: 20210 (very few on stuff)
-% 26130 (20239??) purple cell by kevin. turns out to be 7o
-% struct('name','37','annotation','ON-OFF DS','cells',[26047 20002 20179 20210 20254 26101 20245 26036 ...
-%                                                      20014 20125 26029 26162 90002 26094 26032 26056 ...
-%                                                      20016 20096 20233 26158 17161 20137 26115 26138 26137 ...
-%                                                      90001 20213 25005 26103 17080 20220 26084 26165 26178]);
+struct('name','8n','annotation','','cells',[20126],...
+	'whole_cells',[20126]); 
+% gallery id: 20126
 
-% purple 
-struct('name','37c','annotation','On-Off DS','cells',[26047 20002 20179 20210 20254 26101 20245 26137 26036 26056]); % 20210
-% green (+Z omni) ventral
-struct('name','37d','annotation','On-Off DS','cells',[20014 20125 26029 26162 90002 26094]); % 20125
-% yellow dorsal
-struct('name','37v','annotation','On-Off DS','cells',[20016 20096 20233 26158 17161 20137 26115 26138 26178]); % 20233
-% red 
-struct('name','37r','annotation','On-Off DS','cells',[90001 20213 25005 26103 17080 20220 26084 26165 26032]); % 20213
+% sOnalpha/M4
+struct('name','8w','annotation','sON\alpha/M4','cells',[26071,17111,26001,26079],...
+	'whole_cells',[17111,26001]); 
+% gallery id: 17111
 
-% OS. bistratified tall ~ tri-stratified
-struct('name','63','annotation','F-mini^{On}','cells',[20181 17140 20208 20178 17097 17114 17084 20140 20129 30003 20071 30002 20019 20011 20005 26057 26191 26148 26141 26125 26089 26068 26028 26027 26023]);
-% 20178
-% irregular bilayer
-struct('name','73','annotation','','cells',[20100 20187 20150 20043 26059 26073 26132]); % 72n 20100
-% regular bilayer, layers further 
-struct('name','72','annotation','','cells',[20166 20221 17069 20074 26124]); % 72w 20221
-% regular bilayer. more at off layer
-struct('name','27','annotation','','cells',[20117 17212 26051 26065]); % 26065
+struct('name','81i','annotation','','cells',[20158,26097,26090],...
+	'whole_cells',[20158]); 
+% gallery id: 20158
 
-% regular bilayer two layer even further
-struct('name','81i','annotation','','cells',[20158 26097 26090]); % 20158
-% 
-struct('name','81o','annotation','','cells',[20069 26052]); % 20069
-% struct('name','81','annotation','','cells',[20158 26097 26090 20069 26052]);
+struct('name','81o','annotation','','cells',[20069],...
+	'whole_cells',[20069,26052]); 
+% gallery id: 20069
+
+struct('name','82n','annotation','','cells',[20167,20197,26072,26058,20161,26052,26076],...
+	'whole_cells',[20161,26072]); 
+% gallery id: 20161
+
+struct('name','82wi','annotation','','cells',[26067,30001,20251],...
+	'whole_cells',[30001]); 
+% gallery id: 30001
 
 % regular bilayer 
-struct('name','82wo','annotation','','cells',[20118 26091]); % 20118
-%
-struct('name','82wi','annotation','','cells',[20251 30001 26067]); % 30001
-% [  26058 ]
-struct('name','82n','annotation','','cells',[26076 26080 20161 26058 26072 20080]); % 20161
-% 
-% irregular bilayer 
-struct('name','915','annotation','','cells',[17009 20197]); % 17009
+struct('name','82wo','annotation','','cells',[20118,26091],...
+	'whole_cells',[20118]);
+% gallery id: 20118
 
-% diffuse bilayer more at off layer
-struct('name','28','annotation','','cells',[20243 20155 20163 20257 26005 20167 26033]); % 20155
-% regular bilayer
-% struct('name','91n','annotation','','cells',[20218 25003 20042 26088 26074]);
-% regular bilayer on-dsgc style
-% struct('name','91w','annotation','','cells',[20081 20020 26135]);
-struct('name','91','annotation','M3','cells',[20218 25003 20042 26088 20081 20020]); % 20218
+struct('name','85','annotation','','cells',[20072,20200,17038,20046,26061,20063,26045,26092],...
+	'whole_cells',[20200,26061,20072]); 
+% gallery id: 20200
 
-% struct('name','orphans','annotation','','cells',[20126 20228]);
-% struct('name','orphans','annotation','','cells',[20126]);
+% M5
+struct('name','9n','annotation','','cells',[26135,26149,26168,26127,20006,26074,20056,20112,20076],...
+	'whole_cells',[20006,20076]); 
+% gallery id: 20112
 
-struct('name','weirdos','annotation','','cells',[17134 20248 26069 26093]);
+% M2
+struct('name','9w','annotation','M2','cells',[20228],...
+	'whole_cells',[20228]); 
+% gallery id: 20228
 
-struct('name','cutoffs','annotation','','cells',[10015 17051 17238 26081 26153 26030 26105 26114 26176 26107 26108]);
-% 10015: 2an
-% 17051: 2aw
-% 17238: 2aw
-% 26081: 85
-% 26153: 63
-% 26030: 28
-% 26105: 6sw
-% 26110: 1~3
-% 26114: 82i
-% 26176: 63
-% 26107: 51
-% 26108: 2an
+% M3
+struct('name','91','annotation','','cells',[20218,26080,20020,20042,25003,20081,26088],...
+	'whole_cells',[20020,20218]); 
+% gallery id: 20218
+
+struct('name','915','annotation','','cells',[20080,26119,26033,17009],...
+	'whole_cells',[20197]); 
+% gallery id: 17009
+
+struct('name','weirdos','annotation','','cells',[17134 20248 26069 26093],...
+	'whole_cells',[]);
+
+struct('name','cutoffs','annotation','','cells',[10015 17051 17238 26081 26153 26030 26105 26114 26176 26107 26108],...
+	'whole_cells',[]);
+
 ];
 
